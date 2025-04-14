88ed80ea46c710ae3dc349a24e8bd0cc89baf4b9
LUCHER4321 <lucher4321@gmail.com>
2025-04-14 16:44:02 -0400

    Update DisplayNew.tsx

--- src/components/DisplayNew.tsx
+++ src/components/DisplayNew.tsx
@@ -7,7 +7,7 @@ export const DisplayNew = ({myNew, hd = false}: DisplayNewProps) => {
             <div>
             </div>
             <p className="w-full text-end">{myNew.date}</p>
-            <div className="flex flex-col-reverse sm:flex-row justify-around gap-4 w-full items-start">
+            <div className="flex flex-col sm:flex-row justify-around gap-4 w-full items-start mt-4">
                 <img src={hd ? myNew.hdurl : myNew.url} alt={myNew.title} className="sm:w-1/3 object-contain"/>
                 <p className="text-justify">{myNew.explanation}</p>
             </div>
