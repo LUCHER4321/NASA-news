c95046a2acbadcc84b79295247bab6a8a4b838ae
LUCHER4321 <lucher4321@gmail.com>
2025-04-14 16:18:35 -0400

    Create New.tsx

---
+++ src/classes/New.tsx
@@ -0,0 +1,9 @@
+export class New {
+    constructor(
+        public title: string,
+        public date: string,
+        public explanation: string,
+        public url: string,
+        public hdurl: string,
+    ) { }
+}
