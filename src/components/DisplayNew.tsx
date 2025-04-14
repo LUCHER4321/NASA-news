da661e883cece5fb20fd111fe1643bd5c40812af
LUCHER4321 <lucher4321@gmail.com>
2025-04-14 16:18:45 -0400

    Create DisplayNew.tsx

---
+++ src/components/DisplayNew.tsx
@@ -0,0 +1,21 @@
+import { New } from "../classes/New";
+
+export const DisplayNew = ({myNew, hd = false}: DisplayNewProps) => {
+    return(
+        <>
+            <h1>{myNew.title}</h1>
+            <div>
+            </div>
+            <p className="w-full text-end">{myNew.date}</p>
+            <div className="flex flex-col-reverse sm:flex-row justify-around gap-4 w-full items-start">
+                <img src={hd ? myNew.hdurl : myNew.url} alt={myNew.title} className="sm:w-1/3 object-contain"/>
+                <p className="text-justify">{myNew.explanation}</p>
+            </div>
+        </>
+    )
+};
+
+interface DisplayNewProps {
+    myNew: New;
+    hd?: boolean;
+}
